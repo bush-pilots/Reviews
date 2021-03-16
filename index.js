8db3e4bb91ad98a5a96af0a1fa7d6f783ec1c0dd
Scott Guinn <guinnscott111@gmail.com>
2021-03-16 13:10:28 -0600

    beginning to configure postgres

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+// const queries = require('../database/index.js');
+const morgan = require('morgan')('dev');
+const PORT = 3000;
+const app = express();
+
+app.use(morgan);
+app.use(cors());
+app.use(express.json());
+// app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// app.get('/api/cows', queries.getAll);
+// app.post('/api/cows', queries.create);
+// app.put('/api/cows/:id', queries.update);
+// app.delete('/api/cows/:id', queries.deleteCow);
+
+app.listen(PORT, () => {
+  console.log(`Server listening at localhost:${PORT}!`);
+});
